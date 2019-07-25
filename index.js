b38bdae4f6c7a67d8701845f03795dd2307dad9a
RichardAbsinAmazon <absinr@amazon.com>
2019-07-25 14:37:33 -0700

    edited message in index.js for test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hey from local file!');
+    res.send('Hey from local file! I can write code here!');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
