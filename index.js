e22129c899ddf24114aca02d20abf3ed9eea79c9
RichardAbsinAmazon <absinr@amazon.com>
2019-07-25 14:47:25 -0700

    added sample static webpage

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-    res.send('Hey from local file! I can write code here!');
-});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
