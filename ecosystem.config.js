4a1cb28ed08d9d7edcfd57e8f16a10991083a7d8
RichardAbsin98 <rabsin@uci.edu>
2019-08-06 19:46:41 -0700

    configured ecosystem file for ImagenWebServer

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     apps: [{
-        name: 'ServerTest',
+        name: 'ImagenWebServer',
         script: './index.js'
     }],
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-34-201-116-83.compute-1.amazonaws.com',
-            key: '~/Desktop/Stuff/ImagenKeyPair.pem',
+            host: 'ec2-3-85-164-59.compute-1.amazonaws.com',
+            key: '~/.ssh/ImagenKeyPair02.pem',
             ref: 'origin/master',
             repo: 'https://github.com/RichardAbsin98/ServerTest.git',
-            path: '/home/ubuntu/ServerTest',
+            path: '/home/ubuntu/ImagenWebServer',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
